f0132ec2e9900f18d506cbac4694eb34587bc8cf
Juan P Lopez <dolcalmi@gmail.com>
2025-10-06 19:02:01 -0500

    refactor: get phone metadata

--- core/api/src/app/authentication/get-phone-metadata.ts
+++ core/api/src/app/authentication/get-phone-metadata.ts
@@ -11,34 +11,26 @@ import { getPhoneProviderVerifyService } from "@/services/phone-provider"
 
 export const getPhoneMetadata = async ({ phone }: { phone: PhoneNumber }) => {
   const { phoneMetadataValidationSettings } = getAccountsOnboardConfig()
+  const isValidationEnabled = phoneMetadataValidationSettings.enabled
 
   const verifyService = getPhoneProviderVerifyService()
   if (verifyService instanceof Error) {
-    if (!phoneMetadataValidationSettings.enabled) {
-      return undefined
-    }
-    return new InvalidPhoneMetadataForOnboardingError()
+    return isValidationEnabled ? new InvalidPhoneMetadataForOnboardingError() : undefined
   }
 
-  const newPhoneMetadata = await verifyService.getCarrier(phone)
-  if (newPhoneMetadata instanceof Error) {
-    if (!phoneMetadataValidationSettings.enabled) {
-      return undefined
-    }
-
-    return new InvalidPhoneMetadataForOnboardingError()
+  const phoneMetadata = await verifyService.getCarrier(phone)
+  if (phoneMetadata instanceof Error) {
+    return isValidationEnabled ? new InvalidPhoneMetadataForOnboardingError() : undefined
   }
 
-  const phoneMetadata = newPhoneMetadata
-
-  if (phoneMetadataValidationSettings.enabled) {
-    const authorizedPhoneMetadata = PhoneMetadataAuthorizer(
-      phoneMetadataValidationSettings,
-    ).authorize(phoneMetadata)
-
+  if (isValidationEnabled) {
     addAttributesToCurrentSpan({
       "login.phoneMetadata": JSON.stringify(phoneMetadata),
     })
+
+    const authorizedPhoneMetadata = PhoneMetadataAuthorizer(
+      phoneMetadataValidationSettings,
+    ).authorize(phoneMetadata)
 
     if (authorizedPhoneMetadata instanceof Error) {
       return new InvalidPhoneForOnboardingError(authorizedPhoneMetadata.name)
