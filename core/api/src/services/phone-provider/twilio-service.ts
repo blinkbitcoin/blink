56eef8cb4b46a4be0fbd50363412f5ecb492997d
blinkbitcoinbot <202112752+blinkbitcoinbot@users.noreply.github.com>
2025-10-08 18:31:50 +0000

    Commit state of `core` at `1f3b927`

---
+++ core/api/src/services/phone-provider/twilio-service.ts
@@ -0,0 +1,270 @@
+import twilio from "twilio"
+import { isAxiosError } from "axios"
+
+import { wrapAsyncFunctionsToRunInSpan } from "../tracing"
+
+import {
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  TWILIO_VERIFY_SERVICE_ID,
+  TWILIO_MESSAGING_SERVICE_ID,
+} from "@/config"
+
+import {
+  ExpiredOrNonExistentPhoneNumberError,
+  InvalidOrApprovedVerificationError,
+  InvalidPhoneNumberPhoneProviderError,
+  InvalidTypePhoneProviderError,
+  MissingTypePhoneProviderError,
+  PhoneCodeInvalidError,
+  PhoneProviderConfigError,
+  PhoneProviderConnectionError,
+  PhoneProviderRateLimitExceededError,
+  PhoneProviderUnavailableError,
+  RestrictedRecipientPhoneNumberError,
+  RestrictedRegionPhoneProviderError,
+  UnknownPhoneProviderServiceError,
+  UnsubscribedRecipientPhoneProviderError,
+  isDisposablePhoneNumber,
+} from "@/domain/phone-provider"
+import { parseErrorMessageFromUnknown } from "@/domain/shared"
+
+import { baseLogger } from "@/services/logger"
+
+export const TWILIO_ACCOUNT_TEST = "AC_twilio_id"
+
+export const TwilioClient = (): IPhoneProviderService | PhoneProviderConfigError => {
+  const accountSid = TWILIO_ACCOUNT_SID
+  if (!accountSid) {
+    return new PhoneProviderConfigError("TWILIO_ACCOUNT_SID is required")
+  }
+
+  const authToken = TWILIO_AUTH_TOKEN
+  if (!authToken) {
+    return new PhoneProviderConfigError("TWILIO_AUTH_TOKEN is required")
+  }
+
+  const verifyService = TWILIO_VERIFY_SERVICE_ID
+  if (!verifyService) {
+    return new PhoneProviderConfigError("TWILIO_VERIFY_SERVICE_ID is required")
+  }
+
+  const messagingServiceSid = TWILIO_MESSAGING_SERVICE_ID
+
+  const client = twilio(accountSid, authToken)
+  const verify = client.verify.v2.services(verifyService)
+
+  const initiateVerify = async ({
+    to,
+    channel,
+    phoneExists,
+  }: {
+    to: PhoneNumber
+    channel: ChannelType
+    phoneExists: boolean
+    ip: IpAddress
+  }): Promise<true | PhoneProviderServiceError> => {
+    try {
+      if (!phoneExists) {
+        const validation = await validateDestination(to)
+        if (validation instanceof Error) {
+          return validation
+        }
+      }
+
+      await verify.verifications.create({ to, channel })
+    } catch (err) {
+      baseLogger.error({ err }, "impossible to send text")
+      return handleCommonErrors(err)
+    }
+
+    return true
+  }
+
+  const sendTemplatedSMS = async ({
+    to,
+    contentSid,
+    contentVariables,
+  }: {
+    to: PhoneNumber
+    contentSid: string
+    contentVariables: Record<string, string>
+  }): Promise<true | PhoneProviderServiceError> => {
+    if (!messagingServiceSid) return true
+
+    try {
+      await client.messages.create({
+        to,
+        contentSid,
+        messagingServiceSid,
+        contentVariables: JSON.stringify(contentVariables),
+      })
+
+      return true
+    } catch (err) {
+      baseLogger.error({ err }, "impossible to send templated sms")
+      return handleCommonErrors(err)
+    }
+  }
+
+  const validateDestination = async (
+    phone: PhoneNumber,
+  ): Promise<true | PhoneProviderServiceError> => {
+    try {
+      if (isDisposablePhoneNumber(phone)) {
+        return new InvalidTypePhoneProviderError("disposable")
+      }
+
+      const lookup = await client.lookups.v2.phoneNumbers(phone).fetch({
+        fields: "line_type_intelligence",
+      })
+
+      if (!lookup.lineTypeIntelligence) {
+        return new MissingTypePhoneProviderError()
+      }
+      if (
+        lookup.lineTypeIntelligence["type"] &&
+        String(lookup.lineTypeIntelligence["type"]) === "nonFixedVoip"
+      ) {
+        return new InvalidTypePhoneProviderError("nonFixedVoip")
+      }
+
+      return true
+    } catch (err) {
+      return handleCommonErrors(err)
+    }
+  }
+
+  const validateVerify = async ({
+    to,
+    code,
+  }: {
+    to: PhoneNumber
+    code: PhoneCode
+  }): Promise<true | PhoneProviderServiceError> => {
+    try {
+      const verification = await verify.verificationChecks.create({ to, code })
+      if (verification.status !== "approved") {
+        return new PhoneCodeInvalidError()
+      }
+
+      return true
+    } catch (err) {
+      baseLogger.error({ err }, "impossible to verify phone and code")
+      if (isAxiosError(err) && err.status === 404) {
+        return new ExpiredOrNonExistentPhoneNumberError(err.message)
+      }
+
+      return handleCommonErrors(err)
+    }
+  }
+
+  const getCarrier = async (phone: PhoneNumber) => {
+    try {
+      const result = await client.lookups.v1
+        .phoneNumbers(phone)
+        .fetch({ type: ["carrier"] })
+
+      // TODO: migration to save the converted value to mongoose instead
+      // of the one returned from twilio
+      // const mappedValue = {
+      //   carrier: {
+      //     errorCode: result.carrier?.error_code,
+      //     mobileCountryCode: result.carrier?.mobile_country_code,
+      //     mobileNetworkCode: result.carrier?.mobile_network_code,
+      //     name: result.carrier?.name,
+      //     type: result.carrier?.type,
+      //   },
+      //   countryCode: result.countryCode,
+      // }
+
+      const phoneMetadata: PhoneMetadata = {
+        carrier: {
+          error_code: `${result.carrier.error_code}`,
+          mobile_country_code: `${result.carrier.mobile_country_code}`,
+          mobile_network_code: `${result.carrier.mobile_network_code}`,
+          name: `${result.carrier.name}`,
+          type: `${result.carrier.type}` as CarrierType,
+        },
+        countryCode: result.countryCode,
+      }
+
+      return phoneMetadata
+    } catch (err) {
+      return handleCommonErrors(err)
+    }
+  }
+
+  return wrapAsyncFunctionsToRunInSpan({
+    namespace: "services.twilio",
+    fns: {
+      getCarrier,
+      validateVerify,
+      initiateVerify,
+      sendTemplatedSMS,
+      validateDestination,
+    },
+  })
+}
+
+const handleCommonErrors = (err: Error | string | unknown) => {
+  const errMsg = parseErrorMessageFromUnknown(err)
+
+  const match = (knownErrDetail: RegExp): boolean => knownErrDetail.test(errMsg)
+
+  switch (true) {
+    case match(KnownTwilioErrorMessages.InvalidPhoneNumber):
+    case match(KnownTwilioErrorMessages.InvalidMobileNumber):
+    case match(KnownTwilioErrorMessages.InvalidPhoneNumberParameter):
+      return new InvalidPhoneNumberPhoneProviderError(errMsg)
+
+    case match(KnownTwilioErrorMessages.InvalidCodeParameter):
+      return new PhoneCodeInvalidError(errMsg)
+
+    case match(KnownTwilioErrorMessages.RestrictedRegion):
+    case match(KnownTwilioErrorMessages.CountryNeedsVetting):
+    case match(KnownTwilioErrorMessages.BlockedRegion):
+      return new RestrictedRegionPhoneProviderError(errMsg)
+
+    case match(KnownTwilioErrorMessages.UnsubscribedRecipient):
+      return new UnsubscribedRecipientPhoneProviderError(errMsg)
+
+    case match(KnownTwilioErrorMessages.BadPhoneProviderConnection):
+      return new PhoneProviderConnectionError(errMsg)
+
+    case match(KnownTwilioErrorMessages.ServiceUnavailable):
+      return new PhoneProviderUnavailableError(errMsg)
+
+    case match(KnownTwilioErrorMessages.RateLimitsExceeded):
+    case match(KnownTwilioErrorMessages.TooManyConcurrentRequests):
+      return new PhoneProviderRateLimitExceededError(errMsg)
+
+    case match(KnownTwilioErrorMessages.FraudulentActivityBlock):
+      return new RestrictedRecipientPhoneNumberError(errMsg)
+
+    case match(KnownTwilioErrorMessages.InvalidOrApprovedVerification):
+      return new InvalidOrApprovedVerificationError(errMsg)
+
+    default:
+      return new UnknownPhoneProviderServiceError(errMsg)
+  }
+}
+export const KnownTwilioErrorMessages = {
+  InvalidPhoneNumber: /not a valid phone number/,
+  InvalidMobileNumber: /not a .*mobile number/,
+  InvalidPhoneNumberParameter: /Invalid parameter `To`/,
+  InvalidCodeParameter: /Invalid parameter: Code/,
+  RestrictedRegion: /has not been enabled for the region/,
+  CountryNeedsVetting: /require use case vetting/,
+  UnsubscribedRecipient: /unsubscribed recipient/,
+  BadPhoneProviderConnection: /timeout of.*exceeded/,
+  BlockedRegion:
+    /The destination phone number has been blocked by Verify Geo-Permissions. .* is blocked for sms channel for all services/i,
+  RateLimitsExceeded: /Max.*attempts reached/,
+  TooManyConcurrentRequests: /Too many concurrent requests/,
+  FraudulentActivityBlock:
+    /The destination phone number has been temporarily blocked by Twilio due to fraudulent activities/,
+  ServiceUnavailable: /Service is unavailable. Please try again/,
+  InvalidOrApprovedVerification:
+    /The requested resource \/(?:v2\/)?Services\/(.*?)\/VerificationCheck was not found/,
+} as const
