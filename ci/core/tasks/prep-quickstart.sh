a6e3b0d5ee37521955f7bc590dd0a9a5d0929a32
basarrcan <basarrcan@gmail.com>
2026-05-07 05:55:24 +0300

    chore(quickstart): add transaction grpc stream service

--- ci/core/tasks/prep-quickstart.sh
+++ ci/core/tasks/prep-quickstart.sh
@@ -2,6 +2,7 @@
 
 export api_digest=$(cat ./api-edge-image/digest | sed 's/:/@/')
 export trigger_digest=$(cat ./api-trigger-edge-image/digest | sed 's/:/@/')
+export transactions_grpc_stream_digest=$(cat ./api-transactions-grpc-stream-edge-image/digest | sed 's/:/@/')
 export notifications_digest=$(cat ./notifications-edge-image/digest | sed 's/:/@/')
 export migrate_digest=$(cat ./api-migrate-edge-image/digest | sed 's/:/@/')
 
@@ -11,6 +12,7 @@ pushd repo/quickstart || exit 1
 
 ./bin/bump-galoy-image-digest.sh "api" "$api_digest"
 ./bin/bump-galoy-image-digest.sh "trigger" "$trigger_digest"
+./bin/bump-galoy-image-digest.sh "transactions_grpc_stream" "$transactions_grpc_stream_digest"
 ./bin/bump-galoy-image-digest.sh "notifications" "$notifications_digest"
 ./bin/bump-mongodb-migrate-image-digest.sh "$migrate_digest"
 ./bin/re-render.sh
